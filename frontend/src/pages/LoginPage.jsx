bddd1019cb2c600ed20b7fc18732d2e3c6a93b2e
Jerome Rodrigo <jeromerodrigo06@gmail.com>
2025-05-15 10:55:43 -0700

    Worked on the login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,3 +1,27 @@
+import styles from './LoginPage.module.css';
+import React from 'react';
+
+
 export default function LoginPage() {
-    return <h1>Login Page</h1>;
+    return (
+        <div className={styles['login-page']}>
+            <div className={styles['form-container']}>
+            <h1 className={styles['header']}>Login Page</h1>
+            <form>
+                <label>
+                    id:
+                    <input type="text" name="username" />
+                </label>
+                <br />
+                <label>
+                    Password:
+                    <input type="password" name="password" />
+                </label>
+                <br />
+                <button type="submit">Login</button>
+            </form>
+            </div>
+        </div>
+
+    );
 }
