d71e8c643894776254968245e50de2af90697d94
Jerome Rodrigo <jeromerodrigo06@gmail.com>
2025-06-24 22:01:37 -0700

    worked on home page

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,9 +1,14 @@
 export default function AboutPage() {
     return (
-        <div className="mt-8 mb-8 mx-4">
-            <h1>About Page</h1>
-            <p>This is a simple nutrition recommender system using TinyLlama.</p>
-            <p>It provides personalized nutrition recommendations based on user inputs and questions.</p>
+        <div> 
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8">
+            <div className="mx-4 p-8 rounded-lg bg-[#2C2C2C] shadow-[0_0_20px_0_rgba(255,0,0,0.5)]"> 
+                <h2 className="text-2xl font-bold text-gray-100 mb-6">About Us</h2>
+                <p className="text-gray-300">
+                    Welcome to our nutrition recommendation platform! We are dedicated to providing personalized nutrition advice based on your unique needs and preferences.
+                </p>
+            </div>
+        </div>
         </div>
     );
 }
